547f0c81a869626fd6fb206b1a96b6de2a734a3d
ttokaev <toktim1709@bk.ru>
2023-11-01 12:08:58 +0300

    add delete/patch mapping

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,13 +56,13 @@ public class UserController {
 		return "/users/edit";
 	}
 
-	@PostMapping("/delete")
+	@DeleteMapping("/delete")
 	public String deleteUser(@ModelAttribute("user") User user) {
 		userService.removeUser(user);
 		return "redirect:/";
 	}
 
-	@PostMapping("/update")
+	@PatchMapping("/update")
 	public String updateUser(@ModelAttribute("user") User user) {
 		userService.updateUser(user);
 		return "redirect:/";
